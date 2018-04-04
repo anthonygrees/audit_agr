755820f44770ac1ae7ebf07de376e0e778651f83
anthonygrees <anthony_rees01@hotmail.com>
2018-04-04 18:09:18 +1000

    Added RedHat

---
+++ attributes/default.rb
@@ -0,0 +1,49 @@
+default['audit']['fetcher'] = 'chef-server'
+default['audit']['reporter'] = 'chef-server-automate'
+default['audit']['profiles'] =
+  case node['platform']
+  when 'centos'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Linux Security Baseline',
+        compliance: 'admin/linux-baseline',
+      },
+      {
+        name: 'CIS CentOS Linux 7 Benchmark Level 1',
+        compliance: 'admin/cis-centos7-level1',
+      },
+    ]
+  when 'ubuntu'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Linux Security Baseline',
+        compliance: 'admin/linux-baseline',
+      },
+      {
+        name: 'CIS Ubuntu Linux 16.04 LTS Benchmark Level 1 - Server',
+        compliance: 'admin/cis-ubuntu16.04lts-level1-server',
+      },
+    ]
+  when 'windows'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Windows Security Baseline',
+        compliance: 'admin/windows-baseline',
+      },
+      {
+        name: 'CIS Microsoft Windows Server 2012 R2 Benchmark Level 1 - Member Server',
+        compliance: 'admin/cis-windows2012r2-level1-memberserver',
+      },
+    ]
+when 'redhat'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Linux Security Baseline',
+        compliance: 'admin/linux-baseline',
+      },
+      {
+        name: 'CIS Red Hat Enterprise Linux 7 Benchmark Level 1 - Server',
+        compliance: 'admin/cis-rhel7-level1-server',
+      },
+    ]
+  end
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe 'audit::default'
+# uncomment to correct DevSec profile issues - Linux or Windows
+# include_recipe 'ejc_compliance::devsec_hardening'
+# # uncomment to correct DevSec profile issues - Centos only
+# include_recipe 'ejc_compliance::cis_l1_hardening'
