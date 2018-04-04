513988a4d2ece9d66e70d4162ec5157b978c46d5
anthonygrees <anthony_rees01@hotmail.com>
2018-04-04 18:15:46 +1000

    add version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures audit_agr'
 long_description 'Installs/Configures audit_agr'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 epends 'audit'
