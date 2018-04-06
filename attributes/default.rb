c60650b1a800d84c1e2fd856a6b2fa647cf3a8ce
anthonygrees <anthony_rees01@hotmail.com>
2018-04-06 16:16:51 +1000

    Update the profiles

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,26 +6,30 @@ default['audit']['profiles'] =
     default['audit']['profiles'] = [
       {
         name: 'DevSec Linux Security Baseline',
-        compliance: 'admin/linux-baseline',
+        compliance: 'workstation-1/linux-baseline',
       },
       {
         name: 'CIS CentOS Linux 7 Benchmark Level 1',
-        compliance: 'admin/cis-centos7-level1',
+        compliance: 'workstation-1/cis-centos7-level1',
       },
     ]
   when 'ubuntu'
     default['audit']['profiles'] = [
       {
         name: 'DevSec Linux Security Baseline',
-        compliance: 'admin/linux-baseline',
+        compliance: 'workstation-1/linux-baseline',
       },
       {
         name: 'CIS Ubuntu Linux 16.04 LTS Benchmark Level 1 - Server',
-        compliance: 'admin/cis-ubuntu16.04lts-level1-server',
+        compliance: 'workstation-1/cis-ubuntu16.04lts-level1-server',
       },
     ]
   when 'windows'
     default['audit']['profiles'] = [
+      {
+        name: 'DevSec Windows Security Baseline',
+        compliance: 'workstation-1/windows-baseline',
+      },
       {
         name: 'CIS Microsoft Windows Server 2012 R2 Benchmark Level 1 - Member Server',
         compliance: 'workstation-1/cis-windows2012r2-level1-memberserver',
@@ -33,6 +37,10 @@ default['audit']['profiles'] =
     ]
   when 'redhat'
     default['audit']['profiles'] = [
+      {
+        name: 'DevSec Linux Security Baseline',
+        compliance: 'workstation-1/linux-baseline',
+      },
       {
         name: 'CIS Red Hat Enterprise Linux 7 Benchmark Level 1 - Server',
         compliance: 'workstation-1/cis-rhel7-level1-server',
