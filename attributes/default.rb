cc29f21bcc90a12f0c181e2a9164c884b2280f50
Anthony Rees <anthony_rees01@hotmail.com>
2018-06-23 21:10:02 +1000

    update admin

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,44 +6,44 @@ default['audit']['profiles'] =
     default['audit']['profiles'] = [
       {
         name: 'DevSec Linux Security Baseline',
-        compliance: 'workstation-1/linux-baseline',
+        compliance: 'admin@example.com/linux-baseline',
       },
       {
         name: 'CIS CentOS Linux 7 Benchmark Level 1',
-        compliance: 'workstation-1/cis-centos7-level1',
+        compliance: 'admin@example.com/cis-centos7-level1',
       },
     ]
   when 'ubuntu'
     default['audit']['profiles'] = [
       {
         name: 'DevSec Linux Security Baseline',
-        compliance: 'workstation-1/linux-baseline',
+        compliance: 'admin@example.com/linux-baseline',
       },
       {
         name: 'CIS Ubuntu Linux 16.04 LTS Benchmark Level 1 - Server',
-        compliance: 'workstation-1/cis-ubuntu16.04lts-level1-server',
+        compliance: 'admin@example.com/cis-ubuntu16.04lts-level1-server',
       },
     ]
   when 'windows'
     default['audit']['profiles'] = [
       {
         name: 'DevSec Windows Security Baseline',
-        compliance: 'workstation-1/windows-baseline',
+        compliance: 'admin@example.com/windows-baseline',
       },
       {
         name: 'CIS Microsoft Windows Server 2012 R2 Benchmark Level 1 - Member Server',
-        compliance: 'workstation-1/cis-windows2012r2-level1-memberserver',
+        compliance: 'admin@example.com/cis-windows2012r2-level1-memberserver',
       },
     ]
   when 'redhat'
     default['audit']['profiles'] = [
       {
         name: 'DevSec Linux Security Baseline',
-        compliance: 'workstation-1/linux-baseline',
+        compliance: 'admin@example.com/linux-baseline',
       },
       {
         name: 'CIS Red Hat Enterprise Linux 7 Benchmark Level 1 - Server',
-        compliance: 'workstation-1/cis-rhel7-level1-server',
+        compliance: 'admin@example.com/cis-rhel7-level1-server',
       },
     ]
   end
