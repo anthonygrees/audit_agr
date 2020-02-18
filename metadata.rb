03ca321530d890a70b17db5bc40a2ff9c40770de
Anthony Rees <anthony_rees01@hotmail.com>
2020-02-18 17:07:41 +1100

    update

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,11 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures audit_agr'
 long_description 'Installs/Configures audit_agr'
-version '2.2.2'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+version '2.2.3'
 
-depends 'audit'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'audit', '7.4.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
