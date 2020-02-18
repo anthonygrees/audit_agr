f6b0abd2f21e2c0e4f84a36edb3d63b0a1b965a8
Anthony Rees <anthony_rees01@hotmail.com>
2020-02-18 17:23:28 +1100

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,7 @@
 #
-# Cookbook:: audit-wrapper
+# Cookbook:: audit_agr
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2018, The Authors, All Rights Reserved.
 
-case node['os']
-when 'linux'
-  if node['platform_family'] == 'rhel'
-    node.override['audit']['profiles'] = [ {"name" => "CIS Red Hat Enterprise Linux 7 Benchmark Level 1 - Server", "compliance" => "admin/cis-rhel7-level1-server"}]
-  else
-    node.override['audit']['profiles'] = [ {"name" => "linux-baseline", "compliance" => "admin/linux-baseline"}]
-  end
-when 'windows'
-  node.override['audit']['profiles'] = [ {"name" => "CIS Microsoft Windows Server 2012 R2 Benchmark Level 1 - Member Server", "compliance" => "admin/cis-windows2012r2-level1-memberserver"}]
-end
-
-include_recipe 'audit'
+include_recipe 'audit::default'
